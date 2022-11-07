e6c1f1390c6c990b1154fffcb188ddff48dd8270
Terry Grimoire <terry.grimoire@gmail.com>
2022-11-07 18:47:17 +0400

    resolving image issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Nav from "./Nav";
 import Navbar from "./Navbar";
 import burger from "../assets/menu.png";
+import logo from "../assets/logo.jpg";
 
 function Burger() {
   const [openBurger, setOpenBurger] = useState(false);
@@ -21,11 +22,7 @@ function Burger() {
         />
       </button>
       <Link to="/">
-        <img
-          src="https://scontent-mad1-2.xx.fbcdn.net/v/t39.30808-6/301443427_556650462998417_8691578651850337839_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=YTz3YDc7BR8AX9iRTp2&tn=061AOveasuvNzIkc&_nc_ht=scontent-mad1-2.xx&oh=00_AfCnQzhGncmeKj3GbvVD10TLVl66mblRC2M121uo6F7-rQ&oe=635FA427"
-          alt="logo de barber902 deux ciseaux"
-          className="logo"
-        />
+        <img src={logo} alt="logo de barber902 deux ciseaux" className="logo" />
       </Link>
       {openBurger && <Nav setOpenBurger={setOpenBurger} />}
       <div className="desktop">
--- src/data/coupeData.js
+++ src/data/coupeData.js
@@ -1,51 +1,64 @@
+import coupe1 from "../assets/coupe1.jpg";
+import coupe2 from "../assets/coupe2.jpg";
+import coupe3 from "../assets/coupe3.jpg";
+import coupe4 from "../assets/coupe4.jpg";
+import coupe5 from "../assets/coupe5.jpg";
+import coupe6 from "../assets/coupe6.jpg";
+import coupe7 from "../assets/coupe7.jpg";
+import coupe8 from "../assets/coupe8.png";
+import coupe9 from "../assets/coupe9.png";
+import coupe10 from "../assets/coupe10.png";
+import coupe11 from "../assets/coupe11.png";
+import coupe12 from "../assets/coupe12.png";
+
 const coupeData = [
   {
     id: 0,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F209202582_490847428860501_5731021029029457932_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D109%26_nc_ohc%3DdW4XsvyCKqYAX9d5jOL%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT-siPZdsLIzRtHmyEsBZ1X9gjPW9lXFcxlM8fIm28xUtg%26oe%3D635CDB49%26_nc_sid%3D86f79a",
+    src: coupe1,
   },
   {
     id: 1,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F51171014_261241188103273_4307378740400136503_n.jpg%3Fstp%3Ddst-jpg_e35%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D101%26_nc_ohc%3DzFu7FJlDQywAX_KhnWq%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT_a7ezMzvVd94CyA_Zryp57m0-f2UCWQNQSOl3f9qzlbg%26oe%3D635E69F7%26_nc_sid%3D86f79a",
+    src: coupe2,
   },
   {
     id: 2,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F73495473_412060333032650_3562203883297619697_n.jpg%3Fstp%3Ddst-jpg_e35%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D102%26_nc_ohc%3DbCyj6Jdr22sAX8MXaFv%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT_d0X6zLH26oEqbkNUoXwZHxPUFJq09TXlpdhZ3PCAK3Q%26oe%3D635CD98D%26_nc_sid%3D86f79a",
+    src: coupe3,
   },
   {
     id: 3,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F88988981_134943131361299_8969463919722836522_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D111%26_nc_ohc%3De3F4LSzDz60AX_RqqM9%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT8-gdpRPCtlFRgYBVum4qjBvDCsgeGoOKLB88XZTU3NGA%26oe%3D635D7E3D%26_nc_sid%3D86f79a",
+    src: coupe4,
   },
   {
     id: 4,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F89058741_646124345949177_5425868627839512100_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D109%26_nc_ohc%3Dcd5WxQMyExQAX_kk_G9%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT9kvheNg4rAkdIf5W0z6QqlOzRgy9i2kq00PVV6FXSoPQ%26oe%3D635E87CD%26_nc_sid%3D86f79a",
+    src: coupe5,
   },
   {
     id: 5,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F101445823_2336408473330121_2704973471363776678_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D105%26_nc_ohc%3D8CS00PdGtwkAX8BGEPv%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT8n8izOpAyeeQ5XC_7o67BUFhnPAH8-KtA_2xjy5YHDEw%26oe%3D635E6C31%26_nc_sid%3D86f79a",
+    src: coupe6,
   },
   {
     id: 6,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F102264849_268787664232861_2514809547607128917_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D101%26_nc_ohc%3DnbUzlWvZdnoAX8F0-qW%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT9PpR5oEJjlYOLa8_Hw1EcUqwPeiKK9SVuBfbEu-6xlOw%26oe%3D635DAD78%26_nc_sid%3D86f79a",
+    src: coupe7,
   },
   {
     id: 7,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F103311546_563119377905556_7324189755175349748_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D100%26_nc_ohc%3DjjB9d2tcVTMAX-aC9jS%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT98io0ZXYNs4P9ik0dz9rH7OQH_1zZXOkSKAmHmZsZx-Q%26oe%3D635DB1C5%26_nc_sid%3D86f79a",
+    src: coupe8,
   },
   {
     id: 8,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F104425943_270426360734461_2098956107704526384_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D101%26_nc_ohc%3D52DTeIzIzFAAX_yyAls%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT-4fGy_piUZTQ7BxvTyrWthMNiypPW_Zn-G3pFj7b8cdg%26oe%3D635D7971%26_nc_sid%3D86f79a",
+    src: coupe9,
   },
   {
     id: 9,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F106071033_755861691826669_2822488490818936411_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D101%26_nc_ohc%3Dw0pxK5xgEFQAX_2n9jQ%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT_i6MPJpRYphfP_PrCAAqWm3B-M08ffuDPMa-b96iQwmg%26oe%3D635E3E0F%26_nc_sid%3D86f79a",
+    src: coupe10,
   },
   {
     id: 10,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F109842881_711024023028019_8749430363758765042_n.jpg%3Fstp%3Ddst-jpg_e35_s1080x1080%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D107%26_nc_ohc%3D5R3jhVzln5UAX9_36oD%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT8ajcX1-P_U8gWKWOQa2KwYzSgO5qtKx78WeOPZg6BocA%26oe%3D635D017E%26_nc_sid%3D86f79a",
+    src: coupe11,
   },
   {
     id: 11,
-    src: "https://scp2.elfsightcdn.com/?url=https%3A%2F%2Finstagram.fmaa6-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F73385849_231468464490233_4218783944677506970_n.jpg%3Fstp%3Ddst-jpg_e35%26_nc_ht%3Dinstagram.fmaa6-1.fna.fbcdn.net%26_nc_cat%3D105%26_nc_ohc%3DowvqY-YftnoAX8ru1Vg%26edm%3DAPU89FABAAAA%26ccb%3D7-5%26oh%3D00_AT9om2asekeUX8fv0oxKq7jDc7OjEAuFPCK0R3iE017P1Q%26oe%3D635D51E7%26_nc_sid%3D86f79a",
+    src: coupe12,
   },
 ];
 export default coupeData;
